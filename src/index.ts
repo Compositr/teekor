aab7eedde2b4d642393dd8e1808f8ea517d7501e
Jim (Compositr) <jimke2000@gmail.com>
2022-05-13 12:19:32 +1000

    Intial commit
    Signed-off-by: Jim (Compositr) <jimke2000@gmail.com>

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import path from "path";
+import helmet from "helmet";
+
+const app = express();
+const port = process.env.PORT ?? 80;
+
+// Middleware
+app.use(helmet());
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+// Full snek
+app.get("/fullsnek.png", (req, res) => {
+  res.sendFile(path.join(__dirname, "../assets/fullsnek.png"));
+});
+
+
+// For gifs
+app.get(/.+\.gif$/i, (req, res) => {
+  res.sendFile(path.join(__dirname, "../assets/snek.gif"));
+});
+
+// For pngs
+app.get(/.+\.png$/i, (req, res) => {
+  res.sendFile(path.join(__dirname, "../assets/snek.png"));
+});
+
+// :)
+app.get(/.+\.secret$/i, (req, res) => {
+  res.sendFile(path.join(__dirname, "../assets/snek.png"));
+});
+
+// Base path
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../assets/html/index.html"));
+});
